053dabe1771164918832c78ec4f3661c59f15308
Robert Dibble <63882038+dibble07@users.noreply.github.com>
2024-02-10 08:12:38 +0000

    Incorporate downtime and turbulence adjustment

--- utils.py
+++ utils.py
@@ -18,10 +18,12 @@ from py_wake.wind_farm_models import All2AllIterative, PropagateDownwind
 
 # loads cost values
 # capex and opex https://atb.nrel.gov/electricity/2023/index
-# technology lifespans - https://atb.nrel.gov/electricity/2023/definitions#costrecoveryperiod
+# technology lifespans https://atb.nrel.gov/electricity/2023/definitions#costrecoveryperiod
+# downtime https://blog.windurance.com/the-truth-about-pitch-system-downtime and https://energyfollower.com/how-long-do-wind-turbines-last/
 CAPEX_GW = (3150 + 3901) / 2 * 1e6
 OPEX_GWy = (102 + 116) / 2 * 1e6
 LIFESPAN = 30
+DOWNTIME = 0.02
 
 
 # load farm models
@@ -69,20 +71,46 @@ def run_sim(wfm=wfm_low, x=wt9_x, y=wt9_y, yaw=0, ws=np.arange(0, 31)):
 
 
 # calculate metrics
-def calc_metrics(sim_res, show=False):
+def calc_metrics(sim_res, sim_res_base, show=False):
+    # unpack values
     nt = len(sim_res.wt)
     power_installed = (
         sim_res.windFarmModel.windTurbines.powerCtFunction.power_ct_tab[0].max()
         / 1e9
         * nt
     )
+
+    # calculate turbulent kinetic energy ratio
+    tke_vel = (
+        ((sim_res.TI_eff * sim_res.ws) ** 2 * sim_res.P / sim_res.P.sum()).sum().values
+    )
+    tke_vel_base = (
+        (
+            (sim_res_base.TI_eff * sim_res_base.ws) ** 2
+            * sim_res_base.P
+            / sim_res_base.P.sum()
+        )
+        .sum()
+        .values
+    )
+    tke_ratio = tke_vel / tke_vel_base
+
+    # calculate metrics of interest
     lcoe = (
         (OPEX_GWy + CAPEX_GW / LIFESPAN)
         * power_installed
-        / (sim_res.aep().sum().values * 1000)
+        / (sim_res.aep().sum().values * (1 - DOWNTIME * tke_ratio) * 1000)
     )
-    cap_fac = sim_res.aep().sum().values / (power_installed * 365.25 * 24) * 100
+    cap_fac = (
+        sim_res.aep().sum().values
+        * (1 - DOWNTIME * tke_ratio)
+        / (power_installed * 365.25 * 24)
+        * 100
+    )
+
+    # print values
     if show:
         print(f"LCoE [USD/MWh]: {lcoe:,.3f}")
-        print(f"Capacity factor [%]: {cap_fac:,.2f}")
+        print(f"Capacity factor [%]: {cap_fac:,.3f}")
+
     return lcoe, cap_fac
