1198062ec6b1073f2ce47b51a0a796bddd66adf7
Robert Dibble <63882038+dibble07@users.noreply.github.com>
2024-02-13 15:08:29 +0000

    Add defaults and expanded to range of directions

--- README.md
+++ README.md
@@ -2,22 +2,18 @@
 Investigation of the impact of holistic control of turbines in a wind farm
 
 ## To investigate
-- Impact of wake steering on yield and turbulence energy (impacts O&M costs and uptime)
 - Curtailment of front turbines as well as steering
 - Sensitivity to direction/wind speed
 - Variation in sensitivity between turbines
 - Sensitivity across sites/turbine types
 - Variable hub height (more for floating turbines)
+- Off design performance
 - Real time steering values
     - CNN
     - Lookup table/surrogate model
     - Low fidelity model
     - LiDAR adjustments
 
-## Considerations
-- Off design performance
-- OCC and O&M
-- Uptime
-- Turbulence impact on O&M and uptime
-
 ## To do
+- Don't aggregate TKE_ratio
+- Paralellise metric calculations
--- utils.py
+++ utils.py
@@ -29,6 +29,9 @@ OPEX_VAR_GWh = _grad / (365.25 * 24)
 LIFESPAN = 30
 DOWNTIME = 0.02
 
+# define default ranges
+WS_DEFAULT = np.arange(0, 31)
+WD_DEFAULT = np.arange(0, 360, 45)
 
 # load farm models
 wfm_high = All2AllIterative(
@@ -61,15 +64,15 @@ wfm_lossless = PropagateDownwind(
 
 
 # run simulation
-def run_sim(wfm=wfm_low, x=wt9_x, y=wt9_y, yaw=0, ws=np.arange(0, 31)):
+def run_sim(wfm=wfm_low, x=wt9_x, y=wt9_y, yaw=0, ws=WS_DEFAULT, wd=WD_DEFAULT):
     sim_res = wfm(
         x=x,
         y=y,
         tilt=0,
         yaw=yaw,
-        n_cpu=None,
+        n_cpu=1,
         ws=ws,
-        wd=270,
+        wd=wd,
     )
     return sim_res
 
@@ -77,39 +80,36 @@ def run_sim(wfm=wfm_low, x=wt9_x, y=wt9_y, yaw=0, ws=np.arange(0, 31)):
 # calculate metrics
 def calc_metrics(sim_res, sim_res_base, show=False):
     # unpack values
-    nt = len(sim_res.wt)
-    power_installed = (
-        sim_res.windFarmModel.windTurbines.powerCtFunction.power_ct_tab[0].max()
-        / 1e9
-        * nt
+    rated_power = (
+        sim_res.windFarmModel.windTurbines.powerCtFunction.power_ct_tab[0].max() / 1e9
     )
 
+    # scaled probabilities
+    P = sim_res.P / sim_res.P.sum()
+    P_base = sim_res_base.P / sim_res_base.P.sum()
+    Sector_frequency = sim_res.Sector_frequency / sim_res.Sector_frequency.sum()
+
     # calculate turbulent kinetic energy ratio
-    tke_vel = (
-        ((sim_res.TI_eff * sim_res.ws) ** 2 * sim_res.P / sim_res.P.sum()).sum().values
-    )
-    tke_vel_base = (
-        (
-            (sim_res_base.TI_eff * sim_res_base.ws) ** 2
-            * sim_res_base.P
-            / sim_res_base.P.sum()
-        )
-        .sum()
-        .values
-    )
+    tke_vel = ((sim_res.TI_eff * sim_res.ws) ** 2 * P).sum().values
+    tke_vel_base = ((sim_res_base.TI_eff * sim_res_base.ws) ** 2 * P_base).sum().values
     tke_ratio = tke_vel / tke_vel_base
 
     # calculate metrics of interest
-    aep = sim_res.aep().sum().values
     uptime = 1 - DOWNTIME * tke_ratio
-    lcoe = (
-        (OPEX_FIXED_GWy + CAPEX_GW / LIFESPAN) * power_installed + OPEX_VAR_GWh * aep
-    ) / (aep * uptime * 1000)
-    cap_fac = aep * uptime / (power_installed * 365.25 * 24)
+    aep = sim_res.aep().sum("ws") * uptime
+    fixed_cost = (OPEX_FIXED_GWy + CAPEX_GW / LIFESPAN) * rated_power * Sector_frequency
+    variable_cost = OPEX_VAR_GWh * aep
+    lcoe = (fixed_cost + variable_cost) / (aep * 1000)
+    cap_fac = aep / (
+        Sector_frequency * rated_power * (sim_res.wt * 0 + 1) * 365.25 * 24
+    )
 
     # print values
     if show:
-        print(f"LCoE [USD/MWh]: {lcoe:,.3f}")
-        print(f"Capacity factor [%]: {100*cap_fac:,.3f}")
+        print(f"AEP [GWh]: {aep.sum():,.3f}")
+        print(f"LCoE [USD/MWh]: {(lcoe * aep * 1000).sum()/(aep * 1000).sum():,.3f}")
+        print(
+            f"Capacity factor [%]: {100*cap_fac.weighted(Sector_frequency).mean():,.3f}"
+        )
 
-    return lcoe, cap_fac
+    return aep, lcoe, cap_fac
